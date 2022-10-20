c02c1bbb9744942bfa5690c05ff45f04154951e4
Mihail <darkserj18@gmail.com>
2022-10-20 12:56:45 +0300

    Declaring functions avg, max, values

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -10,4 +10,19 @@ public class App
     {
         System.out.println( "Hello World!" );
     }
+
+    static void avg(){
+
+        System.out.println("avg");
+    }
+
+    static void max(){
+
+        System.out.println("max");
+    }
+
+    static void values(){
+
+        System.out.println("values");
+    }
 }
